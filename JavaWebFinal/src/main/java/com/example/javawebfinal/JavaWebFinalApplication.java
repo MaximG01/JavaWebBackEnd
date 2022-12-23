a306bcca15ec0e179f5a358bcb81a7f2cede497d
MaximG01 <102536881+MaximG01@users.noreply.github.com>
2022-12-22 22:39:36 -0500

    Initial Backend?

---
+++ JavaWebFinal/src/main/java/com/example/javawebfinal/JavaWebFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.javawebfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebFinalApplication.class, args);
+    }
+
+}
