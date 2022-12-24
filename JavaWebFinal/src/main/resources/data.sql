fe31a6f98bdaddb486823c5b4883634d8e944521
MaximG01 <102536881+MaximG01@users.noreply.github.com>
2022-12-23 21:08:01 -0500

    updated naming conventions

--- JavaWebFinal/src/main/resources/data.sql
+++ JavaWebFinal/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 ;
 
-INSERT INTO GunDescriptionList ( THE_NAME, DESCRIPTION, COUNTRY_OF_ORIGIN, YEAR_MADE, CREATOR, RATE_OF_FIRE, BULLET_TYPE, THE_CLASS, FACT1, FACT2, FACT3)
-VALUES ('Ak-47', 'The AK-47, officially known as the Avtomat Kalashnikova, also known as the Kalashnikov or just AK, is a gas-operated assault rifle.',
+INSERT INTO GunDescriptionList (description, country_of_origin, year_made , creator, rate_of_fire, bullet_type, class, fact1, fact2, fact3)
+VALUES ( 'The AK-47, officially known as the Avtomat Kalashnikova, also known as the Kalashnikov or just AK, is a gas-operated assault rifle.',
         'Soviet Union (Russia)', '1947', 'Mikhail Kalashnikov', '600rds/minute', '7.62x39mm (SMG)', 'Automatic Assault Riffle', 'Probably the most widespread firearm in the world',
          'China is the worlds largest ak-47 producer.', 'It is present on 2 flags (Mozambique and Hezbollah)');
 
